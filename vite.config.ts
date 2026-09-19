effbc93cb5635ae8f07826ca1bf9a73b88e9de79
Sumanthkmsuman <sumanthkmsuman@gmail.com>
2026-09-19 22:21:59 +0530

    Enable SPA mode for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       spa: {
         prerender: {
           outputPath: "/index.html",
-          crawlLinks: false,
+          crawlLinks: false, 
         },
       },
     }),
